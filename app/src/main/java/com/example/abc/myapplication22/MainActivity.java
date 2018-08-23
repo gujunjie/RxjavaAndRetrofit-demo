92e764b49be2a0858cbed99156f49a04b40dc3d2
gujunjie <571095488@qq.com>
2018-08-23 17:30:42 +0800

    change

--- app/src/main/java/com/example/abc/myapplication22/MainActivity.java
+++ app/src/main/java/com/example/abc/myapplication22/MainActivity.java
@@ -12,13 +12,17 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -38,11 +42,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_showmessage)
     TextView tvShowmessage;
 
+    Retrofit retrofit;
+
+    UserService userService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+         retrofit = new Retrofit.Builder()
+                .baseUrl("http://apicloud.mob.com/user/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+         userService = retrofit.create(UserService.class);
 
     }
 
@@ -68,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_register:
-              retrofitAndRxjavaWithRigister();
+              retrofitAndRxjavaWithFlatMap();
                 break;
             case R.id.btn_login:
                 retrofitAndRxjavaWithLogin();
@@ -78,12 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void retrofitAndRxjavaWithRigister() {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://apicloud.mob.com/user/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-        UserService userService = retrofit.create(UserService.class);
+
 
         userService.register("2783fa3f54cfa", etUsername.getText().toString(), etPassword.getText().toString())
                 .subscribeOn(Schedulers.newThread())
@@ -125,13 +135,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void retrofitAndRxjavaWithLogin()
     {
-        Retrofit retrofit=new Retrofit.Builder()
-                .baseUrl("http://apicloud.mob.com/user/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
 
-        UserService userService=retrofit.create(UserService.class);
 
         userService.login("2783fa3f54cfa", etUsername.getText().toString(), etPassword.getText().toString())
                 .subscribeOn(Schedulers.io())
@@ -166,4 +170,59 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    public void retrofitAndRxjavaWithFlatMap()
+    {
+
+
+        userService.register("2783fa3f54cfa", etUsername.getText().toString(), etPassword.getText().toString())
+                .subscribeOn(Schedulers.io())//在io线程处理网络请求
+                .observeOn(AndroidSchedulers.mainThread())//在主线程处理ui
+                .doOnNext(new Consumer<Register>() {
+                    @Override
+                    public void accept(Register register) throws Exception {
+                        if(register.getRetCode().equals("200"))
+                        {
+                            Toast.makeText(MainActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                            tvShowmessage.setText("注册成功"+register.getMsg());
+                        }else
+                        {
+                            Toast.makeText(MainActivity.this,"注册失败",Toast.LENGTH_SHORT).show();
+                            tvShowmessage.setText(register.getMsg());
+                        }
+                    }
+                }).observeOn(Schedulers.io())//切换到io线程再次发送网络请求
+                .flatMap(new Function<Register, ObservableSource<Login>>() {
+                    @Override
+                    public ObservableSource<Login> apply(Register register) throws Exception {
+                        if(!register.getRetCode().equals("200"))
+                            return Observable.empty();//清除被观察者，即注册失败就不能自动登陆，下面的代码不会执行
+
+                        return userService.login("2783fa3f54cfa", etUsername.getText().toString(), etPassword.getText().toString());
+                    }
+                }).observeOn(AndroidSchedulers.mainThread())//切换到主线程更新ui
+                .subscribe(new Consumer<Login>() {
+                    @Override
+                    public void accept(Login login) throws Exception {
+                        if(login.getRetCode().equals("200"))
+                        {
+                            Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                            tvShowmessage.setText("登陆成功"+login.getMsg());
+                            Log.d(TAG, "uid= "+login.getResult().getUid());
+                            Log.d(TAG, "token="+login.getResult().getToken());
+                        }else
+                        {
+                            Toast.makeText(MainActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                            tvShowmessage.setText("登陆失败"+login.getMsg());
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CompositeDisposable disposable=new CompositeDisposable();
+        disposable.dispose();
+    }
 }
